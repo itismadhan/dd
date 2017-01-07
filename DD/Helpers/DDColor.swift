b33b12f8f60b18ba41dfc0f52014fb3476906fed
Madhan Padmanabhan <madhan.padmanabhan@citrix.com>
2017-01-07 10:53:11 -0800

    [DD-1] - Initial Bootstrap
    * Assembled story board with layout mods.
    * Added cocoapods: AlamoFire, SwiftyJSON
    * Added assets.
    * Created Helper class for colors
    * Created ExploreViewController and FavoritesViewController

---
+++ DD/Helpers/DDColor.swift
@@ -0,0 +1,14 @@
+//
+//  DDColor.swift
+//  DD
+//
+//  Created by Madhan Padmanabhan on 1/7/17.
+//  Copyright © 2017 madhan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct DDColor {
+    static let Red = UIColor(red:0.98, green:0.00, blue:0.20, alpha:1.0)
+}
